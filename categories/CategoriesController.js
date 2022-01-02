a52a9586d71abf941858598260523827897144e6
luizaugustotpires@gmail.com <luizaugustotpires@gmail.com>
2022-01-02 11:10:31 -0300

    register categories

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,25 @@
 const express = require('express')
+const category = require('./categories')
+const slugify = require('slugify')
 const router = express.Router()
 
 router.get('/admin/categories/new',(req,res)=>{
     res.render("admin/categories/new")
 })
 
-router.get('/admin/categories/new',(req,res)=>{
-    res.send("Criar uma nova categoria")
+router.post('/categories/save',(req,res)=>{
+   let title = req.body.title
+   
+   if (title != undefined) {
+       category.create({
+           title:title,
+           slug:slugify(title)
+       }).then(()=>{
+           res.redirect('/')
+       })
+    }else{
+       res.redirect('/admin/categories/new')
+   }
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const app = express()
 app.set('view engine','ejs')
 
 app.use(express.static("public"))
+
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
