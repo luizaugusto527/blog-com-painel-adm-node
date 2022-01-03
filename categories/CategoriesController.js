8e21c4c8674d2e5cf6c52b6e4e13ae07923b96ca
luizaugustotpires@gmail.com <luizaugustotpires@gmail.com>
2022-01-03 09:56:51 -0300

    edit category

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -15,7 +15,7 @@ router.post('/categories/save', (req, res) => {
             title: title,
             slug: slugify(title)
         }).then(() => {
-            res.redirect('/')
+            res.redirect('/admin/categories')
         })
     } else {
         res.redirect('/admin/categories/new')
@@ -51,5 +51,30 @@ router.post("/categories/delete", (req, res) => {
         res.redirect("/admin/categories")
     }
 })
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    let id = req.params.id
+    if (isNaN(id)) {
+        res.redirect('admin/categories')
+    }
+    category.findByPk(id).then(category => {
+        if (category != undefined) {
+            res.render("admin/categories/edit", { category: category })
+        } else {
+            res.redirect("/admin/categories")
+        }
+    }).catch(erro => {
+        res.redirect('/admin/categories')
+    })
+})
+
+router.post("/categories/update", (req, res) => {
+    let id = req.body.id
+    let title = req.body.title
+
+    category.update({ title: title, slug: slugify(title) }, { where: { id: id } })
+        .then(() => res.redirect("/admin/categories"))
+})
+
 
 module.exports = router
